b814a523fca8565469d269f20f29a676c15760d6
Dominik Berner <dominik.berner@gmail.com>
2017-11-03 15:32:34 +0100

    working example for contest

--- contest/main.cpp
+++ contest/main.cpp
@@ -28,6 +28,23 @@ class IdePrinter :
 
 };
 
+TEST(UnitTest, test1)
+{
+  EXPECT_FALSE(true);
+}
+
+TEST(UnitTest, test2)
+{
+  EXPECT_TRUE(true);
+}
+
+TEST(UnitTest, test3)
+{
+  EXPECT_TRUE(false);
+}
+
+
+
 
 int main(int argc, char** argv)
 {
