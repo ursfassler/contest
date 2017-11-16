a33d3b2a714d184e22131fb01cb9b5223e23a372
Dominik Berner <dominik.berner@bbv.ch>
2017-11-16 11:02:48 +0100

    Using ide_printer in example project
    setting compilation flags for googletests

--- contest/main.cpp
+++ contest/main.cpp
@@ -1,20 +1,4 @@
 #include "gmock/gmock.h"
 #include "ide_printer.h"
 
-TEST(UnitTest, test1) { EXPECT_FALSE(true); }
-
-TEST(UnitTest, test2) { EXPECT_TRUE(true); }
-
-TEST(UnitTest, test3) { EXPECT_TRUE(false); }
-
-int main(int argc, char **argv) {
-  testing::InitGoogleMock(&argc, argv);
-
-  ::testing::TestEventListeners &listeners =
-      ::testing::UnitTest::GetInstance()->listeners();
-
-  delete listeners.Release(listeners.default_result_printer());
-  listeners.Append(new IdePrinter());
-
-  return RUN_ALL_TESTS();
-}
+int main(int argc, char **argv) {}
--- example/cmake/unittest.cpp
+++ example/cmake/unittest.cpp
@@ -1,5 +1,5 @@
 #include "gmock/gmock.h"
-//#include "ide_printer.h"
+#include <ide_printer.h>
 
 TEST(UnitTest, test1) { EXPECT_FALSE(true); }
 
@@ -8,14 +8,13 @@ TEST(UnitTest, test2) { EXPECT_TRUE(true); }
 TEST(UnitTest, test3) { EXPECT_TRUE(false); }
 
 int main(int argc, char **argv) {
-::testing::InitGoogleTest(&argc, argv);
-      	/*testing::InitGoogleMock(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
 
   ::testing::TestEventListeners &listeners =
       ::testing::UnitTest::GetInstance()->listeners();
 
   delete listeners.Release(listeners.default_result_printer());
   listeners.Append(new IdePrinter());
-*/
+
   return RUN_ALL_TESTS();
 }
