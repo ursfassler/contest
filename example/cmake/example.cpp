eec6285ae23880670607af0558f831ca36b91220
Dominik Berner <dominik.berner@bbv.ch>
2017-11-21 07:29:22 +0100

    Squash me: example for output

--- example/cmake/example.cpp
+++ example/cmake/example.cpp
@@ -1,3 +1,5 @@
 // This is a stub file as an example for a regular project file
 
+#include <iostream>
+
 int main(int, char **) { return 0; }
--- example/cmake/unittest.cpp
+++ example/cmake/unittest.cpp
@@ -1,11 +1,12 @@
 #include "gmock/gmock.h"
 #include <ide_printer.h>
+#include <iostream>
 
 TEST(UnitTest, Failing_Test) { EXPECT_FALSE(true); }
 
 TEST(UnitTest, Succeeding_Test) { EXPECT_TRUE(true); }
 
-TEST(UnitTest, Succeeding_Test_2) { EXPECT_TRUE(false); }
+TEST(UnitTest, Succeeding_Test_2) { EXPECT_FALSE(false); }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -15,6 +16,11 @@ int main(int argc, char **argv) {
 
   delete listeners.Release(listeners.default_result_printer());
   listeners.Append(new IdePrinter());
+
+  // C:\Code\contest\example\cmake\example.cpp(6): error C2065: 'blord':
+  // undeclared identifier
+  std::cout << "C:\\Code\\contest\\example\\cmake\\example.cpp(6:6): error "
+               "C2065: 'uttt': undeclared identifier\n";
 
   return RUN_ALL_TESTS();
 }
