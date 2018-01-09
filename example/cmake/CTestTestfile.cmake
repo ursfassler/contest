c357eb017e36eeab372669033318df24c86336c0
Dominik Berner <dominik.berner@gmail.com>
2018-01-09 11:32:21 +0100

    Using cmake-generator expressions for installing

---
+++ example/cmake/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/users/bernerd/Code/tiny-tdd/example/cmake
+# Build directory: /home/users/bernerd/Code/tiny-tdd/example/cmake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TARGET "contest_example_cmake" "COMMAND" "contest_example_cmake_Tests")
+subdirs(googletest)
